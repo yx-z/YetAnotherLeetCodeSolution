8812d1df6a5d35de46c80ec1774be160475cef11
yzhng223 <yzhng223@illinois.edu>
2019-07-04 19:09:23 +0800

    Sol152

--- src/median/Sol152.kt
+++ src/median/Sol152.kt
@@ -3,6 +3,28 @@ package median
 class Sol152 {
 
     fun maxProduct(nums: IntArray): Int {
-        TODO()
+        if (nums.isEmpty()) {
+            return 0
+        }
+
+        val l = nums.size
+        val max = Array(l) { 0 }
+        max[l - 1] = nums[l - 1]
+        val min = Array(l) { 0 }
+        min[l - 1] = nums[l - 1]
+
+        for (i in l - 2 downTo 0) {
+            max[i] = max(nums[i], nums[i] * max[i + 1], nums[i] * min[i + 1])
+            min[i] = min(nums[i], nums[i] * max[i + 1], nums[i] * min[i + 1])
+        }
+
+        return max.max()!!
     }
+
+
+    fun <T : Comparable<T>> max(vararg ts: T) = ts.max()
+        ?: throw NoSuchElementException("no max value")
+
+    fun <T : Comparable<T>> min(vararg ts: T) = ts.min()
+        ?: throw NoSuchElementException("no min value")
 }
