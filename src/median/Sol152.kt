fbc5747945c64431c63221967bfc81898f49e482
yzhng223 <yzhng223@illinois.edu>
2019-07-04 19:10:49 +0800

    S

--- src/median/Sol152.kt
+++ src/median/Sol152.kt
@@ -14,6 +14,9 @@ class Sol152 {
         min[l - 1] = nums[l - 1]
 
         for (i in l - 2 downTo 0) {
+            // we see that max/min[i] only depends on max/min[i + 1]
+            // so we can use O(1) space tracking next max/min only
+            // left as exercise
             max[i] = max(nums[i], nums[i] * max[i + 1], nums[i] * min[i + 1])
             min[i] = min(nums[i], nums[i] * max[i + 1], nums[i] * min[i + 1])
         }
