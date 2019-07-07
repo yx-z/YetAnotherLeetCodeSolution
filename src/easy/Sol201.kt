eb6b6fbe43db8beacc380f7902f9a40f705f513b
yzhng223 <yzhng223@illinois.edu>
2019-07-07 20:27:28 +0800

    Sol201

---
+++ src/easy/Sol201.kt
@@ -0,0 +1,11 @@
+package easy
+
+class Sol201 {
+
+    fun rangeBitwiseAnd(m: Int, n: Int): Int =
+        if (m == n) {
+            m
+        } else {
+            rangeBitwiseAnd(m / 2, n / 2) shl 1
+        }
+}
