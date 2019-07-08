dd65beeacf2af7547a8c3323e884292ecbd1963b
yzhng223 <yzhng223@illinois.edu>
2019-07-08 20:20:52 +0800

    Sol240

---
+++ src/median/Sol240.kt
@@ -0,0 +1,25 @@
+package median
+
+class Sol240 {
+
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+        if (matrix.isEmpty()) {
+            return false
+        }
+
+        val rows = matrix.size
+        val cols = matrix[0].size
+
+        var colIdx = 0
+        var rowIdx = rows - 1
+        while (colIdx < cols && rowIdx >= 0) {
+            val curr = matrix[rowIdx][colIdx]
+            when {
+                curr == target -> return true
+                curr > target -> rowIdx--
+                else -> colIdx++
+            }
+        }
+        return false
+    }
+}
