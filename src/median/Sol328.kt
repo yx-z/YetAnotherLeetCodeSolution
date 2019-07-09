33f232cba80d8ba414fe1c51e05973e15cbcf7eb
yzhng223 <yzhng223@illinois.edu>
2019-07-09 20:06:04 +0800

    Sol328

---
+++ src/median/Sol328.kt
@@ -0,0 +1,32 @@
+package median
+
+class Sol328 {
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+        override fun toString() = `val`.toString()
+    }
+
+    fun oddEvenList(head: ListNode?): ListNode? {
+        val odd = ListNode(0)
+        val even = ListNode(0)
+        var oddCurr = odd
+        var evenCurr = even
+        var curr = head
+        while (curr != null) {
+            oddCurr.next = curr
+            oddCurr = oddCurr.next!!
+            curr = curr.next
+            if (curr == null) {
+                break
+            } else {
+                evenCurr.next = curr
+                evenCurr = evenCurr.next!!
+                curr = curr.next
+            }
+        }
+        oddCurr.next = even.next
+        evenCurr.next = null
+        return odd.next
+    }
+}
