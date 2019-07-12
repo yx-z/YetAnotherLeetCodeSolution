6ad94d0526b1fcd53ab6fcfb4fd1c17e5d3ce75a
yzhng223 <yzhng223@illinois.edu>
2019-07-12 14:15:24 +0800

    Sol494

---
+++ src/median/Sol494.kt
@@ -0,0 +1,27 @@
+package median
+
+import kotlin.math.abs
+
+class Sol494 {
+
+    fun findTargetSumWays(nums: IntArray, S: Int): Int {
+        val n = nums.size
+        val s = nums.sum()
+        if (s < abs(S)) {
+            return 0
+        }
+        val dp = Array(n) { i ->
+            Array(s + 1) {
+                if (i == n - 1 && nums[i] == it) (if (it == 0) 2 else 1)
+                else 0
+            }
+        }
+        for (i in n - 2 downTo 0) {
+            for (j in 0..s) {
+                dp[i][j] = dp[i + 1][abs(j - nums[i])] +
+                        (if (j + nums[i] <= s) dp[i + 1][j + nums[i]] else 0)
+            }
+        }
+        return dp[0][abs(S)]
+    }
+}
