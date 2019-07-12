cffa4829c7c5fa342ad35a32dfe8c84dd3eb4dd0
yzhng223 <yzhng223@illinois.edu>
2019-07-12 21:48:10 +0800

    Sol900

---
+++ src/median/Sol900.kt
@@ -0,0 +1,21 @@
+package median
+
+class Sol900Kt {
+    class RLEIterator(val A: IntArray) {
+        val size = A.size
+        var idx = 0
+
+        fun next(n: Int): Int {
+            var x = n
+            while (idx < size && A[idx] < x) {
+                x -= A[idx]
+                idx += 2
+            }
+            if (idx >= size) {
+                return -1
+            }
+            A[idx] -= x
+            return A[idx + 1]
+        }
+    }
+}
