4a8b6fdf35691ce59a4210af26356f28631f1099
yzhng223 <yzhng223@illinois.edu>
2019-07-20 07:57:51 +0800

    Sol42

---
+++ src/hard/Sol42.kt
@@ -0,0 +1,28 @@
+package hard
+
+import kotlin.math.min
+
+class Sol42 {
+
+    fun trap(height: IntArray): Int {
+        val n = height.size
+        if (n <= 2) return 0
+        val left = IntArray(n)
+        left[0] = 0
+        for (i in 1 until n) {
+            left[i] = if (height[i] >= height[left[i - 1]]) i else left[i - 1]
+        }
+        val rigt = IntArray(n)
+        rigt[n - 1] = n - 1
+        for (i in n - 2 downTo 0) {
+            rigt[i] = if (height[i] >= height[rigt[i + 1]]) i else rigt[i + 1]
+        }
+        var area = 0
+        for (i in 1 until n - 1) {
+            if (left[i] != i && rigt[i] != i) {
+                area += min(height[left[i]], height[rigt[i]]) - height[i]
+            }
+        }
+        return area
+    }
+}
