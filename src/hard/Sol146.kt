dd66b0af5042d867bdff5aa0eee8f6008e4c8c9b
yzhng223 <yzhng223@illinois.edu>
2019-07-20 18:59:11 +0800

    Sol146 Update

--- src/hard/Sol146.kt
+++ src/hard/Sol146.kt
@@ -1,16 +1,52 @@
 package hard
 
-import java.util.*
-
 class Sol146 {
 
     class LRUCache(val capacity: Int) {
-        val data = HashMap<Int, Int>()
-        val keys = LinkedList<Int>()
 
-        fun get(key: Int) = if (keys.contains(key)) {
+        class MyList {
+
+            class Node(val k: Int) {
+                var pre: Node? = null
+                var nex: Node? = null
+            }
+
+            var size = 0
+            val seen = HashMap<Int, Node>()
+            val head = Node(-1)
+            var tail = head
+
+            fun remove(k: Int) {
+                val n = seen[k]!!
+                if (n == tail) {
+                    tail = tail.pre!!
+                }
+                n.pre!!.nex = n.nex
+                n.nex?.pre = n.pre
+                seen.remove(k)
+                size--
+            }
+
+            fun append(k: Int) {
+                tail.nex = Node(k).apply {
+                    pre = tail
+                    seen[k] = this
+                }
+                tail = tail.nex!!
+                size++
+            }
+
+            fun contains(v: Int) = seen.contains(v)
+
+            fun first() = head.nex!!.k
+        }
+
+        val data = HashMap<Int, Int>()
+        val keys = MyList()
+
+        fun get(key: Int) = if (data.containsKey(key)) {
             keys.remove(key)
-            keys.add(key)
+            keys.append(key)
             data[key]!!
         } else {
             -1
@@ -21,10 +57,11 @@ class Sol146 {
             if (keys.contains(key)) {
                 keys.remove(key)
             }
-            keys.add(key)
+            keys.append(key)
             if (keys.size > capacity) {
-                data.remove(keys[0])
-                keys.removeAt(0)
+                val k0 = keys.first()
+                data.remove(k0)
+                keys.remove(k0)
             }
         }
     }
