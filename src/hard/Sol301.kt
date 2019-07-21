e7bda3b921863b68b5f998beb5b748e73ed5c34a
yzhng223 <yzhng223@illinois.edu>
2019-07-21 21:28:12 +0800

    Sol301

---
+++ src/hard/Sol301.kt
@@ -0,0 +1,38 @@
+package hard
+
+import java.util.*
+
+class Sol301 {
+    val ans = HashSet<String>()
+
+    fun removeInvalidParentheses(s: String): List<String> {
+        for (i in 0..s.length) {
+            s.toCharArray().remove(i)
+            if (ans.isNotEmpty()) break
+        }
+        return ans.toList()
+    }
+
+    fun CharArray.remove(count: Int, start: Int = 0) {
+        if (count == 0) {
+            if (isValid()) ans.add(filterNot { it == '_' }.joinToString(""))
+        } else {
+            if (start + count <= size) {
+                copyOf().apply { this[start] = '_' }
+                    .remove(count - 1, start + 1)
+                remove(count, start + 1)
+            }
+        }
+    }
+
+    fun CharArray.isValid(): Boolean {
+        val stack = Stack<Char>()
+        forEach {
+            when (it) {
+                '(' -> stack.push('(')
+                ')' -> if (stack.isEmpty()) return false else stack.pop()
+            }
+        }
+        return stack.isEmpty()
+    }
+}
