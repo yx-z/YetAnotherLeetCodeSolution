2fe179d8b70da8de98bc2cc9cc15e97df5e30225
yzhng223 <yzhng223@illinois.edu>
2019-07-23 09:53:51 +0800

    Sol973

--- src/median/Sol973.kt
+++ src/median/Sol973.kt
@@ -5,14 +5,10 @@ import java.util.*
 class Sol973 {
 
     fun kClosest(points: Array<IntArray>, K: Int): Array<IntArray> {
-        val res = arrayOfNulls<IntArray>(K)
-        val pq = PriorityQueue<IntArray>(K, Comparator { p1, p2 ->
-            p1.dist() - p2.dist()
-        })
-        points.forEach { pq.add(it) }
-        for (i in 0 until K) res[i] = pq.remove()
-        return res.requireNoNulls()
+        val dist = { p: IntArray -> p[0] * p[0] + p[1] * p[1] }
+        val pq = PriorityQueue<IntArray>(K, Comparator { p, q ->
+            dist(p) - dist(q)
+        }).apply { points.forEach { add(it) } }
+        return (1..K).map { pq.remove() }.toTypedArray()
     }
-
-    fun IntArray.dist() = this[0] * this[0] + this[1] * this[1]
 }
