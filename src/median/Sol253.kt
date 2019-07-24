2ddd42aad689d2fa2a6e17bdd581be637d6507f5
yzhng223 <yzhng223@illinois.edu>
2019-07-24 15:44:59 +0800

    Sol253

--- src/median/Sol253.kt
+++ src/median/Sol253.kt
@@ -3,10 +3,10 @@ package median
 class Sol253 {
     class Interval(var start: Int = 0, var end: Int = 0)
 
-    fun minMeetingRooms(intervals: Array<Interval>): Int {
-        if (intervals.isEmpty()) return 0
-        val arr = IntArray(intervals.map { it.end }.max()!! + 1)
-        intervals.forEach { for (i in it.start..it.end) arr[i]++ }
+    fun minMeetingRooms(ints: Array<Interval>): Int {
+        if (ints.isEmpty()) return 0
+        val arr = IntArray(ints.map { it.end }.max()!! + 1)
+        ints.forEach { (it.start..it.end).forEach { arr[it]++ } }
         return arr.max()!!
     }
 }
