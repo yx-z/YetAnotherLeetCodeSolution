6e5a5ed8428877f086c3c1cf32e6c5330ddc7677
yzhng223 <yzhng223@illinois.edu>
2019-07-30 14:39:47 +0800

    Sol410 BinSearch

--- src/hard/Sol410.kt
+++ src/hard/Sol410.kt
@@ -16,7 +16,7 @@ class Sol410 {
             dp[j - 1][j] = max(dp[j - 2][j - 1], ns[j - 1])
             for (i in j until n) {
                 // find a cut k in j - 2 until i : split ns[0..k] into j - 1
-                // chunks and ns[k + 1..i] into the j-th chunk : minimizes
+                // chunks and ns[k + 1. .i] into the j-th chunk : minimizes
                 // the max subarray sum and store it in dp[i][j]
                 dp[i][j] = (j - 2 until i)
                     .map { k -> max(dp[k][j - 1], dp[i][1] - dp[k][1]) }.min()!!
@@ -25,5 +25,40 @@ class Sol410 {
         // O(mn^2)
         // min possible max subarray sum ns[0 until n] split into m chunks
         return dp[n - 1][m]
+    }
+
+    // O(log(ns.sum() - ns.max()!!) * n). This solution performs on Int or Long
+    // better than dp does. But for unbounded integers, this can be slower.
+    // reference: https://leetcode.com/problems/split-array-largest-sum/discuss/89817/Clear-Explanation%3A-8ms-Binary-Search-Java
+    fun splitArrayBinSearch(ns: IntArray, m: Int): Int {
+        val n = ns.size
+        val max = ns.max()!!
+        val sum = ns.sum()
+        if (m == 1) return sum
+        var lo = max
+        var hi = sum
+        val valid = { mid: Int ->
+            var count = 1
+            var total = 0
+            var res = true
+            for (i in ns) {
+                total += i
+                if (total > mid) {
+                    total = i
+                    count++
+                    if (count > m) {
+                        res = false
+                        break
+                    }
+                }
+            }
+            res
+        }
+        while (lo <= hi) {
+            val mid = lo + (hi - lo) / 2
+            if (valid(mid)) hi = mid - 1
+            else lo = mid + 1
+        }
+        return lo
     }
 }
