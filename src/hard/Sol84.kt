ce1321ae771144fff81ea6aa86442cb1819dba7e
yzhng223 <yzhng223@illinois.edu>
2019-08-01 19:36:50 +0800

    Sol84

--- src/hard/Sol84.kt
+++ src/hard/Sol84.kt
@@ -5,35 +5,22 @@ import kotlin.math.max
 
 class Sol84 {
 
-    fun largestRectangleArea(heights: IntArray): Int {
-        var maxArea = 0
-        val stack = Stack<Int>()
-        var p = 0
-        while (p < heights.size) {
-            if (stack.isEmpty()) {
-                stack.push(p)
-                p++
+    fun largestRectangleArea(hs: IntArray): Int {
+        val n = hs.size
+        val s = Stack<Int>()
+        var i = 0
+        var m = 0
+        while (i <= n) {
+            val h = if (i == n) 0 else hs[i]
+            if (s.isEmpty() || h >= hs[s.peek()]) {
+                s.push(i)
+                i++
             } else {
-                val top = stack.peek()
-                if (heights[p] >= heights[top]) {
-                    stack.push(p)
-                    p++
-                } else {
-                    val height = heights[stack.pop()]
-                    val leftLessMin = if (stack.isEmpty()) -1 else stack.peek()
-                    val rightLessMin = p
-                    val area = (rightLessMin - leftLessMin - 1) * height
-                    maxArea = max(area, maxArea)
-                }
+                val top = s.pop()
+                val area = hs[top] * if (s.isEmpty()) i else (i - 1 - s.peek())
+                m = max(m, area)
             }
         }
-        while (stack.isNotEmpty()) {
-            val height = heights[stack.pop()]
-            val leftLessMin = if (stack.isEmpty()) -1 else stack.peek()
-            val rightLessMin = heights.size
-            val area = (rightLessMin - leftLessMin - 1) * height
-            maxArea = max(area, maxArea)
-        }
-        return maxArea
+        return m
     }
 }
