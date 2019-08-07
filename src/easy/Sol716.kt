1e4181bc1b84cfaf008efc1e6c994488ae12ca52
yzhng223 <yzhng223@illinois.edu>
2019-08-07 22:25:08 +0800

    716

--- src/easy/Sol716.kt
+++ src/easy/Sol716.kt
@@ -1,26 +1,22 @@
 package easy
 
 import java.util.*
+import kotlin.Int.Companion.MIN_VALUE
 
 class Sol716 {
 
     class MaxStack {
         private val numStack: Stack<Int> = Stack()
-        private val maxStack: Stack<Int> =
-            Stack<Int>().apply { add(Int.MIN_VALUE) }
+        private val maxStack: Stack<Int> = Stack<Int>().apply { add(MIN_VALUE) }
 
         fun push(x: Int) {
-            if (x >= maxStack.peek()) {
-                maxStack.push(x)
-            }
+            if (x >= maxStack.peek()) maxStack.push(x)
             numStack.push(x)
         }
 
         fun pop(): Int {
             val ret = numStack.pop()
-            if (maxStack.peek() == ret) {
-                maxStack.pop()
-            }
+            if (maxStack.peek() == ret) maxStack.pop()
             return ret
         }
 
@@ -31,17 +27,9 @@ class Sol716 {
         fun popMax(): Int {
             val ret = maxStack.pop()
             val tmp = Stack<Int>()
-            while (numStack.peek() != ret) {
-                tmp.push(numStack.pop())
-            }
+            while (numStack.peek() != ret) tmp.push(numStack.pop())
             numStack.pop()
-            while (tmp.isNotEmpty()) {
-                val x = tmp.pop()
-                if (x >= maxStack.peek()) {
-                    maxStack.push(x)
-                }
-                numStack.push(x)
-            }
+            while (tmp.isNotEmpty()) push(tmp.pop())
             return ret
         }
     }
