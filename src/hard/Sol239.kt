790484d1094bffbfde0fec3cc44de0c276e362e4
yzhng223 <yzhng223@illinois.edu>
2019-08-28 10:15:59 -0400

    239

--- src/hard/Sol239.kt
+++ src/hard/Sol239.kt
@@ -4,6 +4,7 @@ import java.util.*
 
 class Sol239 {
 
+    // O(n log k)
     fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
         val n = nums.size
         if (n == 0 || k > n) return IntArray(0)
@@ -17,5 +18,30 @@ class Sol239 {
             res.add(heap.peek())
         }
         return res.toIntArray()
+    }
+
+    // O(n)
+    fun maxSlidingWindow2(nums: IntArray, k: Int): IntArray {
+        val n = nums.size
+        if (n == 0 || k > n) {
+            return IntArray(0)
+        }
+        val res = IntArray(n - k + 1)
+        var idx = 0
+        val dequeue = ArrayDeque<Int>()
+        for (i in nums.indices) {
+            while (dequeue.isNotEmpty() && dequeue.peekFirst() < i - k + 1) {
+                dequeue.removeFirst()
+            }
+            while (dequeue.isNotEmpty() && nums[dequeue.peekLast()] < nums[i]) {
+                dequeue.removeLast()
+            }
+            dequeue.add(i)
+            if (i >= k - 1) {
+                res[idx] = nums[dequeue.peekFirst()]
+                idx++
+            }
+        }
+        return res
     }
 }
