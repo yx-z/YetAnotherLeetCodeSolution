095fa120675948cdae5de4e2bc771e5d7c2d9ffa
yzhng223 <yzhng223@illinois.edu>
2019-08-30 12:17:15 -0400

    399

--- src/median/Sol399.kt
+++ src/median/Sol399.kt
@@ -3,37 +3,37 @@ package median
 class Sol399 {
 
     fun calcEquation(
-        equations: List<List<String>>,
-        values: DoubleArray,
-        queries: List<List<String>>
+        eqs: List<List<String>>,
+        vs: DoubleArray,
+        qs: List<List<String>>
     ): DoubleArray {
-        val mem = HashMap<String, HashMap<String, Double>>()
-        for (i in equations.indices) {
-            val (s, e) = equations[i]
-            val v = values[i]
-            if (s !in mem) mem[s] = HashMap()
-            if (e !in mem) mem[e] = HashMap()
-            mem[s]!![s] = 1.0
-            mem[e]!![e] = 1.0
-            mem[s]!![e] = v
-            mem[e]!![s] = 1 / v
+        val G = HashMap<String, HashMap<String, Double>>()
+        for (i in eqs.indices) {
+            val (s, e) = eqs[i]
+            val v = vs[i]
+            if (s !in G) G[s] = HashMap()
+            if (e !in G) G[e] = HashMap()
+            G[s]!![s] = 1.0
+            G[e]!![e] = 1.0
+            G[s]!![e] = v
+            G[e]!![s] = 1 / v
         }
         val visited = HashSet<String>()
         fun build(s: String) {
             if (s !in visited) {
                 visited.add(s)
-                mem[s]!!.keys.filterNot { it in visited }.forEach { build(it) }
-                mem[s]!!.keys.filterNot { it == s }.forEach { e ->
-                    mem[e]!!.keys.filterNot { it == s }.forEach { o ->
-                        mem[s]!![o] = mem[s]!![e]!! * (mem[e]!![o]!!)
-                        mem[o]!![s] = 1 / mem[s]!![o]!!
+                G[s]!!.keys.filterNot { it in visited }.forEach { build(it) }
+                G[s]!!.keys.filterNot { it == s }.forEach { e ->
+                    G[e]!!.keys.filterNot { it == s }.forEach { o ->
+                        G[s]!![o] = G[s]!![e]!! * (G[e]!![o]!!)
+                        G[o]!![s] = 1 / G[s]!![o]!!
                     }
                 }
             }
         }
-        for (s in mem.keys) build(s)
-        return queries.map { (s, e) ->
-            if (s in mem && e in mem[s]!!) mem[s]!![e]!! else -1.0
+        G.keys.forEach { build(it) }
+        return qs.map { (s, e) ->
+            if (s in G && e in G[s]!!) G[s]!![e]!! else -1.0
         }.toDoubleArray()
     }
 }
