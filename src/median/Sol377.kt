9495bc9264315514251b4093b86d690d7a07188d
yzhng223 <yzhng223@illinois.edu>
2019-09-10 20:40:08 -0400

    377

--- src/median/Sol377.kt
+++ src/median/Sol377.kt
@@ -4,11 +4,8 @@ class Sol377 {
 
     fun combinationSum4(nums: IntArray, t: Int): Int {
         // dp[i]: # of ways elems in nums summing up to i
-        val dp = IntArray(t + 1)
-        for (i in 1..t) {
-            dp[i] = if (i in nums) 1 else 0
-            nums.filter { i - it > 0 }.forEach { dp[i] += dp[i - it] }
-        }
+        val dp = IntArray(t + 1).apply { this[0] = 1 }
+        for (i in 1..t) nums.filter { i >= it }.forEach { dp[i] += dp[i - it] }
         return dp[t]
     }
 }
