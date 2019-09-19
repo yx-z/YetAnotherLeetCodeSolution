f8cf4c0142cdb001ae8d667348a47f3821ed0832
yzhng223 <yzhng223@illinois.edu>
2019-09-19 11:22:47 -0400

    714

--- src/median/Sol714.kt
+++ src/median/Sol714.kt
@@ -8,10 +8,11 @@ class Sol714 {
         val n = ps.size
         if (n == 0) return 0
         val P = Array(n) { IntArray(2) { Int.MIN_VALUE / 2 } }
+        P[0][0] = 0
         P[0][1] = -ps[0] - fee
         for (i in 1 until n) {
             P[i][0] = max(P[i - 1][0], P[i - 1][1] + ps[i])
-            P[i][1] = max(P[i - 1][1], -ps[i] - fee)
+            P[i][1] = max(P[i - 1][1], P[i - 1][0] - ps[i] - fee)
         }
         return max(0, P[n - 1][0])
     }
