7d68e079dd50b477323b820d92ab939d1492a438
yzhng223 <yzhng223@illinois.edu>
2019-09-19 12:43:02 -0400

    309

--- src/median/Sol309.kt
+++ src/median/Sol309.kt
@@ -27,4 +27,20 @@ class Sol309 {
         }
         return profit[n - 1]
     }
+
+    fun redo(prices: IntArray): Int {
+        val n = prices.size
+        if (n <= 1) return 0
+        // now n >= 2
+        val P = Array(n) { IntArray(2) { Int.MIN_VALUE / 2 } }
+        P[0][0] = 0
+        P[0][1] = -prices[0]
+        P[1][0] = max(P[0][0], P[0][1] + prices[1])
+        P[1][1] = max(P[0][1], -prices[1])
+        for (i in 2 until n) {
+            P[i][0] = max(P[i - 1][0], P[i - 1][1] + prices[i])
+            P[i][1] = max(P[i - 1][1], P[i - 2][0] - prices[i])
+        }
+        return P[n - 1][0]
+    }
 }
