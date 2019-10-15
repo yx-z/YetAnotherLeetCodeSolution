7acc0e03e4cdbee127690f2df22d6ff758138b5b
yzhng223 <yzhng223@illinois.edu>
2019-10-15 17:49:04 -0400

    1115

---
+++ src/median/Sol1115.java
@@ -0,0 +1,32 @@
+package median;
+
+import java.util.concurrent.Semaphore;
+
+public class Sol1115 {
+
+    class FooBar {
+        private int n;
+        private Semaphore s1 = new Semaphore(1);
+        private Semaphore s2 = new Semaphore(0);
+
+        public FooBar(int n) {
+            this.n = n;
+        }
+
+        public void foo(Runnable printFoo) throws InterruptedException {
+            for (int i = 0; i < n; i++) {
+                s1.acquire();
+                printFoo.run();
+                s2.release();
+            }
+        }
+
+        public void bar(Runnable printBar) throws InterruptedException {
+            for (int i = 0; i < n; i++) {
+                s2.acquire();
+                printBar.run();
+                s1.release();
+            }
+        }
+    }
+}
