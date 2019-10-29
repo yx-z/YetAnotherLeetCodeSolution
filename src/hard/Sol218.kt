00e0f64a95b0d4c59da1b8f6b2de30a2ee7a1bb6
yzhng223 <yzhng223@illinois.edu>
2019-10-28 23:46:47 -0400

    218

--- src/hard/Sol218.kt
+++ src/hard/Sol218.kt
@@ -1,28 +1,36 @@
-package Hard
+package hard
 
 import java.util.*
 
 class Sol219 {
 
-    data class Point(val x: Int, val h: Int, val start: Boolean = true)
+    data class Point(val x: Int, val h: Int, val isStart: Boolean = true)
 
     fun getSkyline(buildings: Array<IntArray>): List<List<Int>> {
         val points = buildings.flatMap { it.toPoints() }.sortedWith(Comparator
         { p1, p2 ->
-            if (p1.x == p2.x)
-                (if (p1.start) -p1.h else p1.h) - (if (p2.start) -p2.h else p2.h)
-            else (p1.x - p2.x)
+            // same type (entering): height descending
+            // same type (leaving): height ascending
+            // different type but same height: enter first
+            // otherwise: x ascending
+            if (p1.x != p2.x) (p1.x - p2.x)
+            else (if (p1.isStart) -p1.h else p1.h) -
+                    (if (p2.isStart) -p2.h else p2.h)
         })
+        // <height, # of points at the height>
         val pq = TreeMap<Int, Int>()
         pq[0] = 1
         var preMax = 0
         val res = ArrayList<List<Int>>()
         points.forEach {
-            if (it.start) pq[it.h] = 1 + (pq[it.h] ?: 0)
+            // update # of points based on the type of point and it.h as key
+            if (it.isStart) pq[it.h] = 1 + (pq[it.h] ?: 0)
             else {
                 if (pq[it.h] == 1) pq.remove(it.h)
                 else pq[it.h] = pq[it.h]!! - 1
             }
+
+            // record to res once max height changes (go up/down)
             val curMax = pq.lastKey()
             if (preMax != curMax) {
                 res.add(listOf(it.x, curMax))
