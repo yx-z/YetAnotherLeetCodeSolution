53fd2fc1df421b0c4f4348037675806e614d16c8
yzhng223 <yzhng223@illinois.edu>
2019-11-04 21:29:45 -0500

    133

--- src/google/Sol133.java
+++ src/google/Sol133.java
@@ -1,34 +1,22 @@
 package google;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Sol133 {
 
     Map<Node, Node> copy = new HashMap<>();
-    Set<Node> visited = new HashSet<>();
 
     public Node cloneGraph(Node node) {
-        copy(node);
-        conn(node);
-        return copy.get(node);
-    }
-
-    private void copy(Node node) {
         if (!copy.containsKey(node)) {
-            copy.put(node, new Node(node.val, new ArrayList<>()));
-            node.neighbors.forEach(this::copy);
+            Node c = new Node(node.val, new ArrayList<>());
+            copy.put(node, c);
+            node.neighbors.stream().map(this::cloneGraph)
+                    .forEach(n -> c.neighbors.add(n));
         }
-    }
-
-    private void conn(Node node) {
-        if (!visited.contains(node)) {
-            visited.add(node);
-            Node c = copy.get(node);
-            for (Node n : node.neighbors) {
-                c.neighbors.add(copy.get(n));
-                if (!visited.contains(n)) conn(n);
-            }
-        }
+        return copy.get(node);
     }
 
     class Node {
