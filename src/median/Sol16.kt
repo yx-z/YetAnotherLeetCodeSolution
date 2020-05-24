c4b90de82e56339093c5e561c969b5fcd7992a6c
yzhng223 <yzhng223@illinois.edu>
2020-05-24 17:32:05 -0400

    Sol16

---
+++ src/median/Sol16.kt
@@ -0,0 +1,40 @@
+package median
+
+import kotlin.math.abs
+
+class Sol16 {
+
+	fun threeSumClosest(nums: IntArray, target: Int): Int {
+		val n = nums.size
+		nums.sort()
+		var minDiff = Int.MAX_VALUE
+		var closestSum = -1
+		for (pivot in 0 until n) {
+			var lo = 0
+			var hi = n - 1
+			while (lo < hi) {
+				when {
+					lo == pivot -> lo++
+					hi == pivot -> hi--
+					else -> {
+						val curSum = nums[pivot] + nums[lo] + nums[hi]
+						val curDiff = abs(curSum - target)
+						if (curDiff == 0) {
+							return target
+						}
+						if (curDiff < minDiff) {
+							minDiff = curDiff
+							closestSum = curSum
+						}
+						if (curSum < target) {
+							lo++
+						} else {
+							hi--
+						}
+					}
+				}
+			}
+		}
+		return closestSum
+	}
+}
