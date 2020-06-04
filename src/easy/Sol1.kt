dfdf5238888eeca2fde7420a0a844ab4705c63b5
yzhng223 <yzhng223@illinois.edu>
2020-06-03 20:21:31 -0400

    More elegant

--- src/easy/Sol1.kt
+++ src/easy/Sol1.kt
@@ -5,7 +5,7 @@ class Sol1 {
 	fun twoSum(nums: IntArray, target: Int): IntArray {
 		val seen = HashMap<Int, Int>()
 		nums.forEachIndexed { i, n ->
-			if (target - n in seen) return intArrayOf(seen[target - n]!!, i)
+			seen[target - n]?.let { return intArrayOf(it, i) }
 			seen[n] = i
 		}
 		TODO()
